71df4935cb3fb4892e8f3e665cb1330b09dcac31
= <=>
2021-06-08 23:30:50 +0700

    create project

---
+++ duantungay_8thang_6/src/main/java/com/example/duantungay_8thang_6/Duantungay8thang6Application.java
@@ -0,0 +1,13 @@
+package com.example.duantungay_8thang_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Duantungay8thang6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Duantungay8thang6Application.class, args);
+    }
+
+}
---
+++ duantungay_8thang_6/src/test/java/com/example/duantungay_8thang_6/Duantungay8thang6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duantungay_8thang_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Duantungay8thang6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
